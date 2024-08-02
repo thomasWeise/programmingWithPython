39ca357d14274c6c9f305274f6d733f1c880916e
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-02 17:31:07 +0800

    hopefully further improved dependency version printing

--- scripts/dependencyVersions.sh
+++ scripts/dependencyVersions.sh
@@ -44,17 +44,17 @@ else
 fi
 
 osAndVersion="$osName $osVersion"
-osAndVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${osAndVersion}")" || true
+osAndVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${osAndVersion}")" || true
 if [ -z "$osAndVersion" ]; then
   osAndVersion="$(( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1)"
-  osAndVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${osAndVersion}")" || true
+  osAndVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${osAndVersion}")" || true
 fi
 if [ -n "$osAndVersion" ]; then
     echo "$osAndVersion"
 fi
 
-linuxVersion="$(uname -mrs)" || true
-linuxVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${linuxVersion}")" || true
+linuxVersion="$(uname -mrs 2>&1)" || true
+linuxVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${linuxVersion}")" || true
 if [ -n "$linuxVersion" ]; then
     echo "$linuxVersion"
 fi
@@ -63,59 +63,59 @@ fi
 echo ""
 
 # Print the python version.
-pythonVersion="$(python3 --version | sed -n 's/.*Python\s*\([.0-9]*\)/\1/p')" || true
-pythonVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${pythonVersion}")" || true
+pythonVersion="$(python3 --version 2>&1 | sed -n 's/.*Python\s*\([.0-9]*\)/\1/p')" || true
+pythonVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${pythonVersion}")" || true
 if [ -n "$pythonVersion" ]; then
     echo "python: $pythonVersion"
 fi
 
-latexgitPyVersion="$(pip freeze | grep latexgit | sed -n 's/.*==*\([.0-9]*\)/\1/p')" || true
-latexgitPyVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${latexgitPyVersion}")" || true
+latexgitPyVersion="$(pip freeze 2>&1 | grep latexgit | sed -n 's/.*==*\([.0-9]*\)/\1/p')" || true
+latexgitPyVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${latexgitPyVersion}")" || true
 if [ -n "$latexgitPyVersion" ]; then
     echo "latexgit_py: $latexgitPyVersion"
 fi
 
 latexgitTexVersion="$(less $scriptDir/../styles/latexgit.sty | sed -n 's/.*\\ProvidesPackage{latexgit}\[[0-9\/]* *\([.0-9]*\) .*\].*/\1/p')" || true
-latexgitTexVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${latexgitTexVersion}")" || true
+latexgitTexVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${latexgitTexVersion}")" || true
 if [ -n "$latexgitTexVersion" ]; then
     echo "latexgit_tex: $latexgitTexVersion"
 fi
 
 pycommonsVersion="$(pip freeze | grep pycommons | sed -n 's/.*==*\([.0-9]*\)/\1/p')" || true
-pycommonsVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${pycommonsVersion}")" || true
+pycommonsVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${pycommonsVersion}")" || true
 if [ -n "$pycommonsVersion" ]; then
     echo "pycommons: $pycommonsVersion"
 fi
 
-pdflatexVersion="$(pdflatex --version | grep 'pdfTeX 3')" || true
-pdflatexVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${pdflatexVersion}")" || true
+pdflatexVersion="$(pdflatex --version 2>&1 | grep 'pdfTeX 3')" || true
+pdflatexVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${pdflatexVersion}")" || true
 if [ -n "$pdflatexVersion" ]; then
     echo "pdflatex: $pdflatexVersion"
 fi
 
-biberVersion="$(biber --version | sed -n 's/.*:\s*\([.0-9]*\)/\1/p')" || true
-biberVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${biberVersion}")" || true
+biberVersion="$(biber --version 2>&1 | sed -n 's/.*:\s*\([.0-9]*\)/\1/p')" || true
+biberVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${biberVersion}")" || true
 if [ -n "$biberVersion" ]; then
     echo "biber: $biberVersion"
 fi
 
-makeglossariesVersion="$(makeglossaries --version | grep Version | sed -n 's/.*Version\s*\([.0-9]*\)/\1/p')" || true
-makeglossariesVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${makeglossariesVersion}")" || true
+makeglossariesVersion="$(makeglossaries --version 2>&1 | grep Version | sed -n 's/.*Version\s*\([.0-9]*\)/\1/p')" || true
+makeglossariesVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${makeglossariesVersion}")" || true
 if [ -n "$makeglossariesVersion" ]; then
     echo "makeglossaries: $makeglossariesVersion"
 fi
 
 makeindexVersion="$(echo '' | makeindex  2>&1 | grep makeindex | sed -n 's/.*version\s*\([.0-9]*\)/\1/p')" || true
-makeindexVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${makeindexVersion}")" || true
+makeindexVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${makeindexVersion}")" || true
 if [ -n "$makeindexVersion" ]; then
     echo "makeindex: $makeindexVersion"
 fi
 
-ghostscriptVersion="$(ghostscript --help | head -n 1 | sed -e 's/GPL//i' | sed -e 's/GhostScript//i')" || true
-ghostscriptVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${ghostscriptVersion}")" || true
+ghostscriptVersion="$(gs --help 2>&1 | head -n 1 | sed -e 's/GPL//i' | sed -e 's/GhostScript//i')" || true
+ghostscriptVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${ghostscriptVersion}")" || true
 if [ -z "$ghostscriptVersion" ]; then
-  ghostscriptVersion="$(ghostscript --version)" || true
-  ghostscriptVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]]+/[[:space:]]/'<<<"${ghostscriptVersion}")" || true
+  ghostscriptVersion="$(gs --version 2>&1)" || true
+  ghostscriptVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${ghostscriptVersion}")" || true
 fi
 if [ -n "$ghostscriptVersion" ]; then
     echo "ghostscript: $ghostscriptVersion"
