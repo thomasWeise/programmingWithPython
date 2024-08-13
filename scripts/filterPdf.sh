c035139cebf74069973b8c12b7c18dd8ad55a71d
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-14 06:03:56 +0800

    improved pdf filtering

--- scripts/filterPdf.sh
+++ scripts/filterPdf.sh
@@ -14,101 +14,96 @@ fileOut="$2"
 
 echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We will filter '$fileIn' to '$fileOut using ghostscript."
 
-tempFileA="$(mktemp)"
-echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We will first filter '$fileIn' to '$tempFileA' using ghostscript."
+tempFileSrc="$(mktemp)"
+echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We will first copy '$fileIn' to '$tempFileSrc'."
+cp "$fileIn" "$tempFileSrc"
+fileSize="$(stat -c%s "$tempFileSrc")"
+oldFileSize=""
+cycle=0
 
-gs -dAntiAliasColorImages=true \
-   -dAntiAliasGrayImages=true \
-   -dAntiAliasMonoImages=true \
-   -dAutoFilterColorImages=false \
-   -dAutoFilterGrayImages=false \
-   -dAutoRotatePages=/None \
-   -dBATCH \
-   -dCannotEmbedFontPolicy=/Error \
-   -dColorConversionStrategy=/LeaveColorUnchanged \
-   -dColorImageFilter=/FlateEncode \
-   -dCompressFonts=true \
-   -dCompressStreams=true \
-   -dDetectDuplicateImages=true \
-   -dDownsampleColorImages=false \
-   -dDownsampleGrayImages=false \
-   -dDownsampleMonoImages=false \
-   -dEmbedAllFonts=true \
-   -dFastWebView=false \
-   -dGrayImageFilter=/FlateEncode \
-   -dHaveTransparency=true \
-   -dMaxBitmap=2147483647 \
-   -dNOPAUSE \
-   -dOptimize=true \
-   -dPassThroughJPEGImages=true \
-   -dPassThroughJPXImages=true \
-   -dPDFSTOPONERROR=true \
-   -dPDFSTOPONWARNING=true \
-   -dPreserveCopyPage=false \
-   -dPreserveEPSInfo=false \
-   -dPreserveHalftoneInfo=false \
-   -dPreserveOPIComments=false \
-   -dPreserveOverprintSettings=false \
-   -dPreserveSeparation=false \
-   -dPreserveDeviceN=false \
-   -dPrinted=false \
-   -dQUIET \
-   -dSAFER \
-   -dSubsetFonts=true \
-   -dUNROLLFORMS \
-   -sDEVICE=pdfwrite \
-   -sOutputFile="$tempFileA" "$fileIn" \
-   -c "<</NeverEmbed [ ]>> setdistillerparams" \
-   -q
+while :
+do
+  cycle=$((cycle+1))
+  tempFileDst="$(mktemp)"
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Now beginning filter cycle $cycle with destination '$tempFileDst'."
 
-echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We now filter '$tempFileA' to '$fileOut' using ghostscript."
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We ghostscript to filter '$tempFileSrc' to '$tempFileDst'."
+  gs -dAntiAliasColorImages=true \
+     -dAntiAliasGrayImages=true \
+     -dAntiAliasMonoImages=true \
+     -dAutoFilterColorImages=false \
+     -dAutoFilterGrayImages=false \
+     -dAutoRotatePages=/None \
+     -dBATCH \
+     -dCannotEmbedFontPolicy=/Error \
+     -dColorConversionStrategy=/LeaveColorUnchanged \
+     -dColorImageFilter=/FlateEncode \
+     -dCompatibilityLevel=1.4 \
+     -dCompressFonts=true \
+     -dCompressStreams=true \
+     -dCreateJobTicket=false \
+     -dDetectDuplicateImages=true \
+     -dDoThumbnails=false \
+     -dDownsampleColorImages=false \
+     -dDownsampleGrayImages=false \
+     -dDownsampleMonoImages=false \
+     -dEmbedAllFonts=true \
+     -dFastWebView=false \
+     -dGrayImageFilter=/FlateEncode \
+     -dHaveTransparency=true \
+     -dMaxBitmap=2147483647 \
+     -dNOPAUSE \
+     -dOptimize=true \
+     -dPassThroughJPEGImages=true \
+     -dPassThroughJPXImages=true \
+     -dPDFSTOPONERROR=true \
+     -dPDFSTOPONWARNING=true \
+     -dPreserveCopyPage=false \
+     -dPreserveEPSInfo=false \
+     -dPreserveHalftoneInfo=false \
+     -dPreserveOPIComments=false \
+     -dPreserveOverprintSettings=false \
+     -dPreserveSeparation=false \
+     -dPreserveDeviceN=false \
+     -dPreserveMarkedContent=false \
+     -dPrinted=false \
+     -dOmitInfoDate=true \
+     -dOmitID=true \
+     -dOmitXMP=true \
+     -dQUIET \
+     -dSAFER \
+     -dSubsetFonts=true \
+     -dUCRandBGInfo=/Remove \
+     -dUNROLLFORMS \
+     -sDEVICE=pdfwrite \
+     -sOutputFile="$tempFileDst" "$tempFileSrc" \
+     -c "<</NeverEmbed [ ]>> setdistillerparams" \
+     -c "/PreserveAnnotTypes [/Link] def" \
+     -q
 
-gs -dAntiAliasColorImages=true \
-   -dAntiAliasGrayImages=true \
-   -dAntiAliasMonoImages=true \
-   -dAutoFilterColorImages=false \
-   -dAutoFilterGrayImages=false \
-   -dAutoRotatePages=/None \
-   -dBATCH \
-   -dCannotEmbedFontPolicy=/Error \
-   -dColorConversionStrategy=/LeaveColorUnchanged \
-   -dColorImageFilter=/FlateEncode \
-   -dCompressFonts=true \
-   -dCompressStreams=true \
-   -dDetectDuplicateImages=true \
-   -dDownsampleColorImages=false \
-   -dDownsampleGrayImages=false \
-   -dDownsampleMonoImages=false \
-   -dEmbedAllFonts=true \
-   -dFastWebView=false \
-   -dGrayImageFilter=/FlateEncode \
-   -dHaveTransparency=true \
-   -dMaxBitmap=2147483647 \
-   -dNOPAUSE \
-   -dOptimize=true \
-   -dPassThroughJPEGImages=true \
-   -dPassThroughJPXImages=true \
-   -dPDFSTOPONERROR=true \
-   -dPDFSTOPONWARNING=true \
-   -dPreserveCopyPage=false \
-   -dPreserveEPSInfo=false \
-   -dPreserveHalftoneInfo=false \
-   -dPreserveOPIComments=false \
-   -dPreserveOverprintSettings=false \
-   -dPreserveSeparation=false \
-   -dPreserveDeviceN=false \
-   -dPrinted=false \
-   -dQUIET \
-   -dSAFER \
-   -dSubsetFonts=true \
-   -dUNROLLFORMS \
-   -sDEVICE=pdfwrite \
-   -sOutputFile="$fileOut" "$tempFileA" \
-   -c "<</NeverEmbed [ ]>> setdistillerparams" \
-   -q
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We filtered '$tempFileSrc' to '$tempFileDst' using ghostscript."
+  oldFileSize="$fileSize"
+  echo "--- the size of the old file '$tempFileSrc': $oldFileSize"
+  fileSize="$(stat -c%s "$tempFileDst")"
+  echo "--- the size of the new file '$tempFileDst': $fileSize"
 
-echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Now deleing '$tempFileA'."
+  if (("$cycle" > 3)) ; then
+    if (("$fileSize" >= "$oldFileSize")) ; then
+      echo "--- We completed $cycle cycles and the new file is not smaller than the old file, so we stop and use the old file."
+      rm "$tempFileDst"
+      break
+    else
+      echo "--- We completed $cycle cycles and the new file is smaller than the old file, so we continue."
+    fi
+  else
+    echo "--- We completed only $cycle cycles, so we continue."
+  fi
 
-rm "$tempFileA"
+  rm "$tempFileSrc"
+  tempFileSrc="$tempFileDst"
+  done
+
+echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Now moving '$tempFileSrc' to '$fileOut'."
+mv "$tempFileSrc" "$fileOut"
 
 echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Successfully finished filtering '$fileIn' to '$fileOut using ghostscript."
